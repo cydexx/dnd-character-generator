035ea95f73bcb9571bdb04728e29c283bf972cef
cydex <cydex201209@gmail.com>
2023-04-24 00:56:23 +0300

    pop-up animations

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react"
 import { motion } from "framer-motion"
 import Loading from "./Loading"
+
+import { ClipboardDocumentIcon } from "@heroicons/react/24/outline"
+
 export default function CharacterCard() {
     const [characters, setCharacters] = useState([])
     const [error, setError] = useState(false)
     const [loading, setLoading] = useState(false)
+    const [showPopup, setShowPopup] = useState(false)
 
     async function generateCharacter(e) {
         e.preventDefault()
@@ -24,18 +28,36 @@ export default function CharacterCard() {
             setLoading(false)
         }
     }
+
     function copyCharactersToClipboard(characters) {
         const characterString = JSON.stringify(characters)
         navigator.clipboard.writeText(characterString)
-        return true
+        setShowPopup(true)
     }
+
     function copyCharacterToClipboard(characters) {
         const characterString = JSON.stringify(
             characters[characters.length - 1]
         )
         navigator.clipboard.writeText(characterString)
-        return true
+        setShowPopup(true)
     }
+
+    useEffect(() => {
+        if (showPopup) {
+            setTimeout(() => {
+                setShowPopup(false)
+            }, 5000)
+        }
+    }, [showPopup])
+
+    useEffect(() => {
+        if (error) {
+            setTimeout(() => {
+                setError(false)
+            }, 10000)
+        }
+    }, [error])
 
     return (
         <div className="flex flex-col items-center justify-center px-20">
@@ -53,9 +75,7 @@ export default function CharacterCard() {
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                     onClick={() =>
-                        copyCharactersToClipboard(characters) &&
-                        alert("All characters copied successfully!") &&
-                        setIsOpen(open)
+                        copyCharactersToClipboard(characters) && setIsOpen(open)
                     }
                     className="mx-2 my-2 disabled:cursor-not-allowed bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-4 py-2 text-xs"
                 >
@@ -67,13 +87,7 @@ export default function CharacterCard() {
                     <Loading />
                 </div>
             ) : null}
-            {error ? (
-                <div className="bg-red-500 text-white p-4 rounded-md text-[15px] text-center">
-                    {error}
-                </div>
-            ) : (
-                <></>
-            )}
+
             <div className="flex flex-col-reverse gap-5">
                 {characters.map((character, index) => (
                     <motion.div
@@ -121,17 +135,45 @@ export default function CharacterCard() {
                         <motion.button
                             whileHover={{ scale: 1.1 }}
                             whileTap={{ scale: 0.9 }}
-                            onClick={() =>
-                                copyCharacterToClipboard(characters) &&
-                                alert("Character copied successfully!")
-                            }
-                            className="mx-2 my-2 disabled:cursor-not-allowed bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-4 py-2 text-xs"
+                            onClick={() => copyCharacterToClipboard(characters)}
+                            className="flex my-2 disabled:cursor-not-allowed bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 font-bold rounded text-white px-3 py-2 text-xs"
                         >
+                            <ClipboardDocumentIcon className="h-5 mr-1" />
                             Copy Character as a JSON
                         </motion.button>
                     </motion.div>
                 ))}
             </div>
+            {showPopup && (
+                <motion.div
+                    initial={{ x: 300, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                        delay: 0.2,
+                        type: "spring",
+                        stiffness: 200,
+                        damping: 20,
+                    }}
+                    className="fixed top-0 right-0 m-4 p-4 bg-green-500 text-white rounded-lg shadow-lg"
+                >
+                    Character(s) copied successfully!
+                </motion.div>
+            )}
+            {error && (
+                <motion.div
+                    initial={{ x: 300, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                        delay: 0.2,
+                        type: "spring",
+                        stiffness: 200,
+                        damping: 20,
+                    }}
+                    className="fixed top-0 right-0 m-4 p-4 bg-red-500 text-white rounded-lg shadow-lg"
+                >
+                    Some error(s) occured while character generating.
+                </motion.div>
+            )}
         </div>
     )
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -9,29 +9,21 @@ import React, { useState } from "react"
 export default function Loading() {
     const [closed, setClosed] = useState(false)
 
-    const handleClose = () => {
-        setClosed(true)
-    }
-    const menuVariants = {
-        open: {
-            opacity: 1,
-            x: 0,
-        },
-        closed: {
-            opacity: 0,
-            x: "-100%",
-        },
-    }
     return (
         <motion.div
-            animate={closed == true ? "open" : "closed"}
-            className={`flex items-center justify-center px-4 lg:px-0 py-12 ${
-                closed == true && "hidden"
-            }`}
+            initial={{ x: 0, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+                delay: 0.2,
+
+                stiffness: 200,
+                damping: 20,
+            }}
+            className={`flex items-center justify-center px-4 lg:px-0 py-12 `}
         >
             <div
                 className={
-                    "transition delay-150 duration-300 ease-in-out  mx-auto py-3 px-4 bg-green-600 md:flex items-center justify-between shadow-md shadow-green-300 rounded"
+                    "transition delay-150 duration-300 ease-in-out  mx-auto py-3 px-4 bg-green-500 md:flex items-center justify-between shadow-md shadow-green-300 rounded"
                 }
             >
                 <div className="sm:flex sm:items-start lg:items-center">
@@ -43,19 +35,13 @@ export default function Loading() {
                             INFO
                         </p>
                     </div>
-                    <div className="flex flex-col">
-                        <p className="text-x  text-white pt-2 sm:pt-0 pb-2 sm:pb-0">
-                            The character is being produced... This process
-                            takes some time as the artificial intelligence
-                            thinks while creating the character. Please Wait
-                            Your character is being created.
+                    <div className="text-md text-white">
+                        <p className=" pt-2 sm:pt-0 pb-2 sm:pb-0">
+                            The character is being produced. This process takes
+                            some time as the AI thinks while creating the
+                            character. Please wait Your character is being
+                            created.
                         </p>
-                        <p className="text-xs  text-white pt-2 sm:pt-0 pb-2 sm:pb-0"></p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-end sm:mt-4 md:mt-0 ml-4">
-                    <div onClick={handleClose} className="cursor-pointer">
-                        <XMarkIcon className="h-6 w-6 text-white hover:text-red-300 active:text-red-500" />
                     </div>
                 </div>
             </div>
